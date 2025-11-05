1c8afc2e02bff4f5141a17aadfdbc1064fce92df
Taxdi <117667973+Taxdi@users.noreply.github.com>
2025-11-05 17:02:54 +0100

    Add interrupt handler for serial input
    Implement interrupt handler for Ctrl+C detection

---
+++ STM32/Ressources/Interruption/interrupt_handler.cpp
@@ -0,0 +1,18 @@
+#include "interrupt_handler.h"
+
+volatile bool interrupted = false;
+
+void initInterruptHandler() {
+    interrupted = false;
+}
+
+void checkInterrupt() {
+    if (Serial.available() > 0) {
+        int c = Serial.peek();
+        if (c == 3) { // ASCII 3 = Ctrl+C
+            Serial.read(); // Consomme le caractère
+            interrupted = true;
+            Serial.println("\r\n[Kernel] Interruption détectée (Ctrl+C)");
+        }
+    }
+}
